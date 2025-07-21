5f7ddbcab3a76cf1bee6e184dc22b77908137782
MdAnasKhan0 <mdanaskhan.201902037@gmail.com>
2025-07-22 00:03:00 +0600

    user regrester controller added.

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,5 @@
-import user from "../models/authModel";
 import {sign} from "jsonwebtoken";
+import User from "../models/authModel";
 
 const maxAge = 3*24*60*60*1000; 
 const createToken = (email, password) => {
@@ -19,7 +19,21 @@ export const register = async(req, res, next) => {
         }
 
         const newUser = await User.create({email, password});
-        res.status(201).json(newUser)
+        res.cookie("jwt", createToken(email, user.id),{
+            maxAge,
+            secure: true,
+            sameSite: "None",
+        });
+        return res.status(201).json({user:{
+            id: newUser.id,
+            email: newUser.email,
+            firstName: newUser.firstName,
+            lastName: newUser.lastName,
+            image: newUser.image,
+            color: newUser.color,
+            profileSetUp: newUser.profileSetUp,
+        },
+    });
     } catch (error) {
         console.log(error)
         return res.status(500).json({ message: "Internal server error" })
