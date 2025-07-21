4c26f87041867a18c20ffb6dfa223c5c92983287
MdAnaskhan0 <mdanaskhan.201902037@gmail.com>
2025-07-21 14:29:49 +0600

    Creating routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
-const  App = () => {
+// Pages
+import Auth from './pages/auth/auth'
+import Chat from './pages/chat/Chat'
+import Profile from './pages/profile/Profile'
+
+const App = () => {
   return (
-    <>
-      <button className='btn'>click Me</button>
-    </>
+    <Routes>
+      {/* Redirect all unknown routes to /auth */}
+      <Route path="*" element={<Navigate to="/auth" replace />} />
+
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/chat" element={<Chat />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
   )
 }
 
-export default  App
+export default App
